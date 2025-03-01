01c67dc6c365a42c9c8b821fb306127539fd8f15
cloudlabmp <110421070+cloudlabmp@users.noreply.github.com>
2025-03-01 18:33:40 +0000

    tflint error fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,11 @@
-variable "region" {}
-variable "instance_type" {}
+variable "region" {
+  description = "AWS region where resources will be deployed"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 1.3.0"
+}
+
+terraform {
   backend "s3" {
     bucket         = "gitops-tf-backend-mpcloudlab"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "GitopsTerraformLocks"
-  }
+
 }
+  }
+
